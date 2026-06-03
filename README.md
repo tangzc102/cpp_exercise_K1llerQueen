ad8d0477592dbc9f095598ed41070511592d52cb
tangzc102 <2123272553@qq.com>
2026-06-03 18:15:43 +0800

    补全README版本说明 + v2新增键盘输入代码

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# cpp_exercise_K1llerQueen
+# cpp\_exercise\_K1llerQueen
+
 This repository very very good
+
+v1.0：C语言基础HelloWorld程序，实现控制台打印输出
+
+v2.0：新增scanf函数，实现键盘输入、读取并打印用户输入内容
+
--- ex01/hello.c
+++ ex01/hello.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(){
-    printf("Hello World\n");
+int main()
+{
+    char buf[128];
+    printf("请输入内容：");
+    scanf("%s",buf);
+    printf("你输入的内容：%s\n",buf);
     return 0;
 }
